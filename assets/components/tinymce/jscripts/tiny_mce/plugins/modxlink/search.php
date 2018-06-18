e94719aaa39a27c07895eabf243a420873165b27
Jens Wittmann <jenswittmann@users.noreply.github.com>
2018-06-18 21:00:44 +0200

    bugfix search for resources on clientconfig
    bugfix search for resources when tinymce used on clientconfig

--- assets/components/tinymce/jscripts/tiny_mce/plugins/modxlink/search.php
+++ assets/components/tinymce/jscripts/tiny_mce/plugins/modxlink/search.php
@@ -4,26 +4,49 @@
  *
  * @package tinymce
  */
-require_once dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))))).'/config.core.php';
+
+require_once dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
 
-$searchMode = $modx->getOption('search-mode',$_REQUEST,'pagetitle');
 $query = $modx->getOption('q',$_REQUEST,'');
+if(strlen($query) < 3) exit();
+
+// System setting defines if we should list links across contexts or not
+$across_contexts = $modx->getOption('tinymcerte.links_across_contexts', null, true);
 
 $c = $modx->newQuery('modResource');
-$c->where(array(
-    $searchMode.':LIKE' => '%'.$query.'%',
+
+// Note, reason we nest the query in two array is because if we are going to filter on context, we want the final query on the form:
+// ((pagetitle or alias) and context). If we don't filter on context, it will make no difference that the where clause has an extra
+// parenthesis
+$where_clause = array(array(
+  'pagetitle:LIKE' => '%'.$query.'%',
+  'OR:alias:LIKE' => '%'.$query.'%',
 ));
 
-$count = $modx->getCount('modResource',$c);
+if (!$across_contexts) {
+  if (isset($_GET['context']) and strlen($_GET['context']) and $_GET['context'] == 'undefined') {
+	$where_clause[] = array(
+      'context_key' => $_GET['context']
+	);
+  }
+}
+
+$c->where($where_clause);
 
 $c->select(array('id','pagetitle','alias'));
 $c->limit(10);
 
 $resources = $modx->getCollection('modResource',$c);
 
+$a = array();
 foreach ($resources as $resource) {
-    echo $resource->get('pagetitle').' ('.$resource->get('id').')|'.$resource->get('id')."\n";
+	$a[] = array(
+		'id' => $resource->get('id')
+		,'pagetitle' => htmlspecialchars($resource->get('pagetitle')) . " (" . $resource->get('id') . ")"
+		,'title' => $resource->get('pagetitle')
+		,'alias' => $resource->get('alias')
+	);
 }
-die();
+exit(json_encode($a));
